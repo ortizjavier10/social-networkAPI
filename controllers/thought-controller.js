32f160413a813219b5cbceafc973f8eada61618b
JAVI <ortiz.javier10@gmail.com>
2020-10-04 17:17:22 -0400

    user controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,20 @@
+const { User, Thought } = require('../models');
+const { model } = require('../models/User');
+
+const thoughtController = {
+
+
+
+
+
+
+};
+
+module.exports = thoughtController;
+
+
+
+
+
+
+
